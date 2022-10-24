7cd572e19be825c792ec792b514e4e077236c1e0
Ricy40 <ricardogarcia.pt@outlook.com>
2022-10-24 19:52:38 +0100

    Combat System Operational, Just needs UI

--- monsters/monster_init.py
+++ monsters/monster_init.py
@@ -9,25 +9,25 @@ def battleMonster(monster):
     phase1 = True
     phase2 = True
     phase3 = True
-    while monster.isAlive() and not monster.isSpared():
-        while phase1:
+    while not monster.isSpared() and monster.isAlive():
+        while phase1 and monster.isAlive():
             print(health.genHealthBar(player.player_health))
             monster.display_health()
             phase1 = monster.phase1()
             if player.player_health < 1:
                 print("Game Over\nYou died!")
-        while phase2:
+        while phase2 and monster.isAlive():
             print(health.genHealthBar(player.player_health))
             monster.display_health()
             phase2 = monster.phase2()
             if player.player_health < 1:
                 print("Game Over\nYou died!")
-        while phase3:
+        while phase3 and monster.isAlive():
             print(health.genHealthBar(player.player_health))
             monster.display_health()
             phase3 = monster.phase3()
             if player.player_health < 1:
-                print("Game Over\nYouDied!")
+                print("Game Over\nYou died!")
 
 
 battleMonster(stephy)
