2e50372056adff47ccac85d98da56febda319d7f
Ricy40 <ricardogarcia.pt@outlook.com>
2022-10-25 17:01:03 +0100

    C3nti the centrifuge

--- items/item_init.py
+++ items/item_init.py
@@ -5,6 +5,7 @@ from items.item import Item
 
 fist = Weapon("Fist", 5, 4, 0.05, """Your bare fists.\nFists have a higher chance of dealing a critical blow.""")
 bunsen = Weapon("Bunsen burner", 12,7,0.05,"A bunsen burner that can shoot fire, capable of dealing considerable damage") 
+
 # Items
 
 map = Item("map", "This seems to be a map of the facility. It's a bit worn and tattered, but it's still readable.")
