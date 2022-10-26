a78488ad656a80c1c0c630c06f2b0167f9d38bb7
Josh Arbourne <arbournej@cardiff.ac.uk>
2022-10-26 12:14:07 +0100

    Fixed a mistake I made fixing a mistake :(

--- items/item_init.py
+++ items/item_init.py
@@ -8,7 +8,7 @@ stethoscope = Weapon("stethoscope", "stethoscope", 6, 4, 0.20, "A small stethosc
 centrifuge = Weapon("centrifuge", "centrifuge", 10, 7, 0.10, "A hefty centrifuge that could potentially be used to whack someone round the head with.")
 testtube = Weapon("testtube", "test tube rocket", 12, 10, 0.05, "A set of Test Tubes that can be fired much like a missile at whoever stands in your way.")
 bunsen = Weapon("bunsen", "bunsen burner", 17, 10, 0.15, "A bunsen burner that spews endless amounts of fire, despite not having a fuel tank.")
-raygun = Weapon("raygun", "nuclear raygun", 50, 30, 0.30, "The test weapon from this secret facility, it appears to fire some sort of nuclear laser beam".)
+raygun = Weapon("raygun", "nuclear raygun", 50, 30, 0.30, "The test weapon from this secret facility, it appears to fire some sort of nuclear laser beam.")
 
 # Items
 
